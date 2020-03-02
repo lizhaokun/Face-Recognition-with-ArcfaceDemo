f73a56919889ab5278ccb5b46a5eb02879f47839
lizhaokun <37857379+lizhaokun@users.noreply.github.com>
2020-03-02 15:22:09 +0800

    Add files via upload

--- app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -1,6 +1,6 @@
 package com.arcsoft.arcfacedemo.common;
 
 public class Constants {
-    public static final String APP_ID = "7UTUXv6B8JXgxiU8j5R2rsaRFM2Y4wNF8wMVW7pPW5BK";
-    public static final String SDK_KEY = "6eYzJEiPP613UYyUnZyMtq2ekRXaLNeKu7hyfKSeqC4d";
+    public static final String APP_ID = "";
+    public static final String SDK_KEY = "";
 }
